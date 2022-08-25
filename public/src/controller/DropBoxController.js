8b9ac2d07d2dee77e5d685c243c4e65c02b12e2c
Guilherme <mttsguil@gmail.com>
2022-08-25 09:23:12 -0300

    add: send archives with Ajax and Promise.all

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -20,11 +20,57 @@ class DropBoxController {
 
         this.inputFilesEl.addEventListener('change', e=> { 
 
-            console.log(e.target.files)
+           
+            this.uploadTask(e.target.files);
 
             this.snackModal.style.display = 'block';
 
         });
+
+    }
+
+    uploadTask(files) { 
+
+        let promises = [];
+
+        [...files].forEach(file => { 
+
+            promises.push(new Promise((resolve, reject) => { 
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => { 
+
+                    try { 
+                        resolve(JSON.parse(ajax.responseText));
+                    } catch (e) { 
+
+                        reject(e);
+
+                    } 
+
+                };
+
+                ajax.onerror = event => { 
+
+                    reject(event);
+
+                };
+
+                let formData = new FormData();
+            
+                formData.append('input-file', file )
+
+                ajax.send(formData);
+
+            }));
+
+        });
+
+        return Promise.all(promises);
+
 
     }
 
