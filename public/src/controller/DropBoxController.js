9a5ba39a7007a5b8be922337d1bc94374cf29135
Guilherme <mttsguil@gmail.com>
2022-08-25 12:32:47 -0300

    add: selecting archives with CTRL or SHIFT

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -414,6 +414,8 @@ class DropBoxController {
         
         `
 
+        this.initEventsLi(li);
+
         return li;
         
         
@@ -438,6 +440,62 @@ class DropBoxController {
 
         });
 
-    }
+    };
+
+    initEventsLi(li){
+
+        li.addEventListener('click', e => {
+            
+            if(e.shiftKey) { 
+
+                let firstLi = this.listFilesEl.querySelector('.selected');
+
+                if (firstLi) { 
+
+                    let indexStart;
+                    let indexEnd;
+                    let lis = li.parentElement.childNodes;
+
+                    lis.forEach((el, index)=> { 
+
+                        if( firstLi === el) indexStart = index;
+                        if(li === el) indexEnd = index;
+
+                    });
+
+                    let index = [indexStart, indexEnd].sort();
+
+                    lis.forEach((el, i) => {
+
+                        if (i >= index[0] && i <= index[1]) { 
+
+                            el.classList.add('selected')
+                        }
+
+
+
+                    });
+
+                    return true;
+                }
+
+            }
+
+            if(!event.ctrlKey) { 
+
+                this.listFilesEl.querySelectorAll('li.selected').forEach(el=> { 
+
+                    el.classList.remove('selected')
+
+
+                });
+
+            }
+
+            li.classList.toggle('selected')
+
+        });
+
+    };
 
 }
