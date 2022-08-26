6cab449a1cf42cd50f610a1283f1effdf60d5473
Guilherme <mttsguil@gmail.com>
2022-08-26 11:41:02 -0300

    add: delete archives firebase, delete pastes firebase and open archive

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -56,6 +56,61 @@ class DropBoxController {
 
     };
 
+    removeFolderTask(ref, name){
+
+        return new Promise((resolve, reject)=> { 
+
+            let folderRef = this.getFirebaseRef(ref + '/' + name);
+
+            folderRef.on('value', snapshot=> { 
+
+                folderRef.off('value')
+
+                snapshot.forEach(item => { 
+
+                    let data = item.val();
+                    data.key = item.key;
+
+                    if(data.type === 'folder') { 
+
+                        this.removeFolderTask(ref + '/' + name, data.name).then(()=> { 
+
+                            resolve({
+                                fields:{
+                                    key: data.key
+                                }
+                            })
+
+                        }).catch(err=> { 
+                            reject(err)
+                        });
+
+                    } else if(data.type) { 
+                        this.removeFile(ref + '/' + name, data.name).then(()=> { 
+
+                            resolve({
+                                fields:{
+                                    key: data.key
+                                }
+                            })
+
+                        }).catch(err=> { 
+                            reject(err)
+                        });
+                    };
+
+                });
+
+                folderRef.remove();
+                
+
+            });
+
+
+        });
+
+    }
+
     removeTask() {
 
         let promises = []
@@ -65,18 +120,56 @@ class DropBoxController {
             let file = JSON.parse(li.dataset.file)
             let key = li.dataset.key;
 
-            let formData = new FormData();
+            promises.push(new Promise((resolve, reject)=> { 
+                if(file.type === 'folder') { 
 
-            formData.append('path', file.path)
-            formData.append('key', key)
+                    this.removeFolderTask(this.currentFolder.join('/'), file.name)
+                    .then(()=> {
 
-            promises.push(this.ajax('/file', 'DELETE', formData))   
+                        resolve ({
+    
+                            fields: { 
+                                key
+                            }
+        
+                        })
+    
+                    });;
+
+                } else if (file.type) { 
+
+                    this.removeFile(this.currentFolder.join('/'), file.name).then(()=> {
+
+                        resolve ({
+    
+                            fields: { 
+                                key
+                            }
+        
+                        })
+    
+                    });
+
+                }
+
+               
+
+               
+
+            }));   
 
             });
 
         return Promise.all(promises);
 
     };
+
+    removeFile(ref, name) { 
+
+        let fileRef = firebase.storage().ref(ref).child(name)
+
+            return fileRef.delete();
+    }
 
     initEvents() {
 
@@ -694,7 +787,7 @@ class DropBoxController {
                 break;
 
                 default:
-                window.open('/file?path=' + file.path)
+                window.open(file.path)
 
 
             };
